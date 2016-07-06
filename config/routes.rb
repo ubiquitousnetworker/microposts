47b9b67256ef670d52668a2aefc84b0a7da35f07
ubiquitousnetworker <ubiquitousnetworker@gmail.com>
2016-07-06 06:46:41 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  #get 'static_pages"home'
   root to: 'static_pages#home'
+  get 'signup',  to: 'users#new'
+  
+  resources :users
+  
+  # get 'users/new'
+  
+  #get 'static_pages"home'
+  # root to: 'static_pages#home'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
