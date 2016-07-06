6c6f1160feb5aa8599a8dab69a488898814f907a
ubiquitousnetworker <ubiquitousnetworker@gmail.com>
2016-07-06 06:54:18 +0000

    add login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
-  
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   
   # get 'users/new'
   
